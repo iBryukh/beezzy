ceb187d892345bb81acc80365907ad8bd3e2d89c
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2016-02-13 16:50:51 +0200

    final schema version, model

--- src/main/java/beezyy/domain/entities/UserEntity.java
+++ src/main/java/beezyy/domain/entities/UserEntity.java
@@ -19,8 +19,14 @@ public class UserEntity {
     @Column(name = "EMAIL")
     private String email;
 
-    @Column(name = "PASS")
+    @Column(name = "PASSWORD")
     private String password;
+
+    @Column(name = "ROLE_ID")
+    private int roleId;
+
+    @Column(name = "ACTIVE")
+    private boolean active;
 
     public int getId() {
         return id;
@@ -46,4 +52,19 @@ public class UserEntity {
         this.password = password;
     }
 
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
