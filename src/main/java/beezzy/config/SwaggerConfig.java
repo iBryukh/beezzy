dce3ed457a576a5116ef669bd0591b28964e5d18
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2016-04-24 04:25:59 +0300

    swagger does not work. shiiiiiit!

---
+++ src/main/java/beezzy/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package beezzy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by oleh on 24.04.2016.
+ */
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_12)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "REST API",
+                "This is a description of API.",
+                "version-1",
+                "API TOS",
+                "bkk@gug.ni",
+                "API License",
+                "API License URL"
+        );
+        return apiInfo;
+    }
+}
